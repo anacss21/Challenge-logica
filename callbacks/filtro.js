fae524760941778a62327bae7d600f994c89d307
Ana Carolina <anacss2198@gmail.com>
2022-03-03 14:08:10 -0300

    filtros

--- callbacks/filtro.js
+++ callbacks/filtro.js
@@ -15,7 +15,17 @@ console.log(
   })
 );
 
-// Filtro de números positivos
+// Filtro de números positivos: Faça um programa que filtra um array
+// de números inteiros e retorne um novo array apenas com os números positivos.
+
+const numeros = [10, 987, -886, 0, 12, 199, -45, -67];
+
+console.log(
+  numeros.filter((positivos) => {
+    return positivos > 0;
+  })
+);
+
 // Filtro de números pares
 // Filtro de nome por letra
 // Filtro de usuários
