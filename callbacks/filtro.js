f476feac378245a054ebc9e146e08b9051f9698c
Ana Carolina <anacss2198@gmail.com>
2022-03-03 13:55:47 -0300

    filtros

--- callbacks/filtro.js
+++ callbacks/filtro.js
@@ -1,4 +1,21 @@
-// Filtro de caracteres
+// Filtro de caracteres: Faça um programa que percorra um
+// array com nomes de cidades e retorne um novo array somente com nomes
+// que contenham até 8 caracteres.
+
+const cidades = [
+  "Salvador",
+  "São Paulo",
+  "Brasilia",
+  "Recife",
+  "Rio de Janeiro",
+];
+
+console.log(
+  cidades.filter((x) => {
+    return x.length <= 8;
+  })
+);
+
 // Filtro de números positivos
 // Filtro de números pares
 // Filtro de nome por letra
