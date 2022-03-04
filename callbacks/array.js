5ee34fc0e0eb592b636d361f798c04a57c1b57fc
Ana Carolina <anacss2198@gmail.com>
2022-03-03 22:11:33 -0300

    array para ser feito

--- callbacks/array.js
+++ callbacks/array.js
@@ -1,5 +1,1 @@
-// Faça um programa que, a partir de um array, cria um novo array com os mesmos elementos
-//mas que todos os elementos comecem com letras maiúsculas e em cada elemento seja adicionado
-// o número do índice correspondente.
-
 const frutas = ["Manga", "UVA", "abacaxi", "banaNA", "MAçã"];
